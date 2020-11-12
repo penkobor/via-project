6b8877b85d6c2569c4d0483c605ede8b4a27a638
Boris Penko <penkoboris95@gmail.com>
2020-11-12 16:05:08 +0100

    adding ms bot

---
+++ back-end/controller.js
@@ -0,0 +1,17 @@
+export class Controller {
+    createTask = () => {
+        return 'task has been created'
+    }
+    deleteTask = () => {
+        return 'task has been deleted'
+    }
+    updateTask = () => {
+        return 'task has been updated'
+    }
+    getTask = () => {
+        return 'task has been recieved'
+    }
+    askAssistant = () => {
+        return 'assistant has been asked '
+    }
+}
--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,36 @@
 var express = require('express');
+const { Controller } = require('./controller');
 var app = express();
+
+var controller = new Controller();
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.get('/todos', function(req, res) {
+  const todos = ['']; // fetch from google calendar API
+  res.send(controller.getTask);
+})
+
+app.post('/todos/create', function(req, res) {
+  // create todo
+  const status = 200;
+  res.send(controller.createTask)
+})
+
+app.post('/todos/delete', function(req, res) {
+  // create todo
+  const status = 200;
+  res.send(controller.deleteTask)
+})
+
+app.post('/todos/update', function(req, res) {
+  // create todo
+  const status = 200;
+  res.send(controller.updateTask)
+})
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
