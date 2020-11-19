2b72fc9ca74d7ae54195d6b01cdb9a0813a25bfb
Boris Penko <penkoboris95@gmail.com>
2020-11-19 14:53:24 +0100

    Added google api

--- back-end/myTsBot/my-assistant/src/index.ts
+++ back-end/myTsBot/my-assistant/src/index.ts
@@ -6,17 +6,86 @@ import * as path from 'path';
 import { config } from 'dotenv';
 const ENV_FILE = path.join(__dirname, '..', '.env');
 config({ path: ENV_FILE });
-
+const fs = require('fs');
+const readline = require('readline');
+const { google } = require('googleapis');
 import * as restify from 'restify';
-
-// Import required bot services.
-// See https://aka.ms/bot-services to learn more about the different parts of a bot.
 import { BotFrameworkAdapter } from 'botbuilder';
-
-// This bot's main dialog.
 import { EchoBot } from './bot';
 
+const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+const TOKEN_PATH = 'token.json';
+fs.readFile('/Users/borispenko/via-project/back-end/myTsBot/my-assistant/src/credentials-2.json', (err, content) => {
+    if (err) return console.log('Error loading client secret file:', err);
+    // Authorize a client with credentials, then call the Google Calendar API.
+    authorize(JSON.parse(content), listEvents);
+});
 
+let oAuth2Client;
+
+function authorize(credentials, callback) {
+    const {client_secret, client_id, redirect_uris} = credentials.installed;
+    oAuth2Client = new google.auth.OAuth2(
+        client_id, client_secret, redirect_uris[0]);
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, (err, token) => {
+      if (err) return getAccessToken(oAuth2Client, callback);
+      oAuth2Client.setCredentials(JSON.parse(token));
+      callback(oAuth2Client);
+    });
+  }
+
+  function getAccessToken(oAuth2Client, callback) {
+    const authUrl = oAuth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: SCOPES
+    });
+    console.log('Authorize this app by visiting this url:', authUrl);
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    rl.question('Enter the code from that page here: ', (code) => {
+      rl.close();
+      oAuth2Client.getToken(code, (err, token) => {
+        if (err) return console.error('Error retrieving access token', err);
+        oAuth2Client.setCredentials(token);
+        // Store the token to disk for later program executions
+        fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+          if (err) return console.error(err);
+          console.log('Token stored to', TOKEN_PATH);
+        });
+        callback(oAuth2Client);
+      });
+    });
+  }
+
+  async function listEvents(auth) {
+    const calendar = google.calendar({version: 'v3', auth});
+    const ids = await calendar.calendarList.list().then(res => {
+        return res.data.items.map(item => item.id)
+    });
+
+    const tomorrow4pm = new Date();
+    tomorrow4pm.setDate(tomorrow4pm.getDate() + 1);
+    const allEvents = [];
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < ids.length; i++) {
+        const events = await calendar.events.list({
+            calendarId: ids[i],
+            timeMin: (new Date()).toISOString(),
+            timeMax: (tomorrow4pm).toISOString(),
+            maxResults: 10,
+            singleEvents: true,
+            orderBy: 'startTime'
+        }).then(res => {
+            return res.data.items.map(e => e.summary)
+        })
+        allEvents.push(events)
+    }
+    // TODO: return as json
+    return allEvents.join()
+  }
 // Create HTTP server.
 const server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, () => {
@@ -65,6 +134,16 @@ server.post('/api/messages', (req, res) => {
         await myBot.run(context);
     });
 });
+
+server.get('/tasks', async (req, res, next) => {
+    console.log('Hello from tasks')
+    // listEvents(oAuth2Client);
+    res.send({
+        lastIntent: await listEvents(oAuth2Client),
+        productToModify: ['taks']
+    });
+    next();
+})
 
 // Listen for Upgrade requests for Streaming.
 server.on('upgrade', (req, socket, head) => {
